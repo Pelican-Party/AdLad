04bf399b02aedb04971ffa562c96e741ab316d81
Jesper van den Ende <jespertheend@gmail.com>
2023-03-07 21:21:14 +0100

    chore: Generate .d.ts file with bundle

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -5,6 +5,7 @@
 import { rollup } from "npm:rollup@3.17.3";
 import { minify } from "npm:terser@5.15.0";
 import { setCwd } from "$chdir_anywhere";
+import * as path from "$std/path/mod.ts";
 setCwd();
 
 /**
@@ -33,4 +34,34 @@ const bundle = await rollup({
 await bundle.write({
 	file: "../dist/AdLad.js",
 	format: "esm",
+});
+
+const tmpDir = await Deno.makeTempDir();
+
+Deno.chdir("..");
+const proc = Deno.run({
+	cmd: [
+		"deno",
+		"run",
+		"--allow-env",
+		"--allow-read",
+		"--allow-write",
+		"npm:typescript@4.9.5/tsc",
+		"-p",
+		"./generateTypes.tsconfig.json",
+		"--outDir",
+		tmpDir,
+	],
+
+	// tsc emits a whooole bunch of type errors that we are just going to ignore
+	// because deno test already does type checking for us
+	stderr: "null",
+	stdout: "null",
+});
+await proc.status();
+
+await Deno.copyFile(path.resolve(tmpDir, "src/AdLad.d.ts"), path.resolve("dist/AdLad.d.ts"));
+
+await Deno.remove(tmpDir, {
+	recursive: true,
 });
